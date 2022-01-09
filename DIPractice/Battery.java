857de53f79ed3f8fcc8d93ae411850db74e8e908
정성원 <37476534+woniwory@users.noreply.github.com>
2022-01-10 01:44:45 +0900

    Create Battery.java

---
+++ DIPractice/Battery.java
@@ -0,0 +1,12 @@
+package DIpractice;
+
+public class Battery {
+    int status;
+
+    public Battery(int status){
+        this.status = status;
+//        Battery라는 객체의 status라는 변수 값은
+//        이제부터 매개변수로 받아온 status의 값과 같게할거야 : 생성자
+    }
+
+}
